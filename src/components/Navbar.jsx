348dc70bb59c5fe986991c2cd64c72bba1ac9738
Rohit Prashar <rohitprashar712@gmail.com>
2025-07-17 19:58:22 +0100

    Adding data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
     return (
         <nav className="bg-emerald-300 text-white px-4 py-3 flex items-center justify-between">
             <h2 className="text-3xl font-bold">
-                <span className="text-black">Rohit</span>
-                <span className="text-black"> Prashar</span>
+                <span className="text-3xl font-bold text-gray-900">Rohit Prashar</span>
             </h2>
+
 
         </nav>
     );
---
+++ src/data/data.jsx
@@ -0,0 +1,29 @@
+import {nanoid} from "nanoid";
+
+export const links = [
+    {id: nanoid(), name: "Home", url: "/"},
+    {id: nanoid(), name: "About", url: "/about"},
+    {id: nanoid(), name: "Projects", url: "/projects"},
+    {id: nanoid(), name: "Contact", url: "/contact"},
+];
+
+
+export const socials = [
+    {id: nanoid(), name: "Github", url: "https://github.com/rohitprashar"},
+    {id: nanoid(), name: "Linkedin", url: "https://www.linkedin.com/in/rohitprashar"},
+    {id: nanoid(), name: "medium", url: "https://twitter.com/rohitprashar"},
+];
+
+export const projects = [
+    {id: nanoid(), name: "Project 1", url: "https://github.com/rohitprashar"},
+    {id: nanoid(), name: "Project 2", url: "https://github.com/rohitprashar"},
+    {id: nanoid(), name: "Project 3", url: "https://github.com/rohitprashar"},
+    {id: nanoid(), name: "Project 4", url: "https://github.com/rohitprashar"},
+];
+
+export const skills = [
+    {id: nanoid(), name: "Java", url: "https://github.com/rohitprashar"},
+    {id: nanoid(), name: "React", url: "https://github.com/rohitprashar"},
+    {id: nanoid(), name: "Javascript", url: "https://github.com/rohitprashar"},
+    {id: nanoid(), name: "Typescript", url: "https://github.com/rohitprashar"},
+];
