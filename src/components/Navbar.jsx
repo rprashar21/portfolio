4d947aead3583502b83a7a7764a73a40555d7ff2
Rohit Prashar <rohitprashar712@gmail.com>
2025-07-17 19:43:34 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="bg-emerald-300 text-white px-4 py-3 flex items-center justify-between">
+            <h2 className="text-3xl font-bold">
+                <span className="text-black">Rohit</span>
+                <span className="text-black"> Prashar</span>
+            </h2>
+
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Sample.jsx
@@ -0,0 +1,8 @@
+export const Sample = () => {
+    return (
+        <>
+            Sample
+        </>
+
+    );
+}
