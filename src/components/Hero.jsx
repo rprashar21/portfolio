365fec73a229fd8b3498c7c12ed1e848c559cecf
Rohit Prashar <rohitprashar712@gmail.com>
2025-07-18 09:27:55 +0100

    hero component setup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+    return (
+        <>
+            <div className="bg-emerald-100 py-24">
+                <div
+                    className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+                    <article>
+                        <h1 className="text-3xl font-bold tracking-wider">I'm Rohit</h1>
+                        <h2 className="text-3xl font-bold tracking-wider">Full Stack Developer</h2>
+                        <p className="mt-4">Hardwork Beats Talent !!</p>
+                    </article>
+
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Hero
